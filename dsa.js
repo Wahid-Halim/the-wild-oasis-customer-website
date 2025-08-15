9b91ce9bf0d063a5a19dfe6d58703ec18f80535b
Wahid <wahidkeo@gmail.com>
2025-08-15 12:04:05 +0430

    loading feature add to cabins page

--- dsa.js
+++ dsa.js
@@ -1,9 +1,36 @@
-const arr = "apple, banana, cherry, date, elderberry".split(", ");
+const { useDebugValue } = require("react");
 
-function printItems(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]); // runs n times
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+  pop() {
+    if (this.length === 0) return undefined;
+
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+
+    this.length--;
+    return lastItem;
   }
 }
 
-printItems(arr);
+const myNewArray = new MyArray();
+console.log(myNewArray.push("apple"));
+console.log(myNewArray.push("orange"));
+console.log(myNewArray.push("banana"));
+
+console.log(myNewArray.get(1), "index");
+console.log(myNewArray.pop());
+console.log(myNewArray);
